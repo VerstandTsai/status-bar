d1f0fabd273e5bc45864372e9aab2015935767ba
Verstand Tsai <hackertsaigenz@gmail.com>
2026-06-22 01:13:04 +0800

    Improve battery

--- src/bar.rs
+++ src/bar.rs
@@ -1,9 +1,11 @@
 use std::io::{prelude::*, stdout};
 
+use crate::battery::Battery;
+
 pub struct Bar {
     width: usize,
     workspace_id: u32,
-    battery_percentage: u32
+    battery: Battery
 }
 
 impl Bar {
@@ -11,13 +13,21 @@ impl Bar {
         Bar {
             width: width,
             workspace_id: 1,
-            battery_percentage: 100
+            battery: Battery::new()
         }
+    }
+
+    fn draw_battery(&self) -> String {
+        let icons0 = ["󰂎", "󰁺", "󰁻", "󰁼", "󰁽", "󰁾", "󰁿", "󰂀", "󰂁", "󰂂", "󰁹"];
+        let icons1 = ["󰢟", "󰢜", "󰂆", "󰂇", "󰂈", "󰢝", "󰂉", "󰢞", "󰂊", "󰂋", "󰂅"];
+        let i = self.battery.percentage / 10;
+        let icon = if self.battery.charging { icons1[i] } else { icons0[i] };
+        format!("{} {}%", icon, self.battery.percentage)
     }
 
     fn draw(&self) {
         let ws = self.workspace_id.to_string();
-        let bat = self.battery_percentage.to_string();
+        let bat = self.draw_battery();
         let pad = " ".repeat(self.width - ws.len() - bat.len());
         print!("\x1b[?25l\r{}{}{}", ws, pad, bat);
         stdout().flush().unwrap();
@@ -28,8 +38,8 @@ impl Bar {
         self.draw();
     }
 
-    pub fn set_battery_percentage(&mut self, percentage: u32) {
-        self.battery_percentage = percentage;
+    pub fn set_battery(&mut self, battery: Battery) {
+        self.battery = battery;
         self.draw();
     }
 }
--- src/battery.rs
+++ src/battery.rs
@@ -1,18 +1,41 @@
 use std::sync::{Arc, Mutex};
 use futures_util::StreamExt;
+use tokio;
 use zbus::Connection;
 use crate::bar::Bar;
 use crate::device::DeviceProxy;
 
+#[derive(Copy, Clone)]
+pub struct Battery {
+    pub percentage: usize,
+    pub charging: bool
+}
+
+impl Battery {
+    pub fn new() -> Battery {
+        Battery {
+            percentage: 100,
+            charging: false
+        }
+    }
+}
+
 pub async fn listen(barc: Arc<Mutex<Bar>>) {
+    let mut battery = Battery::new();
     let conn = Connection::system().await.expect("Cannot connect to DBus");
     let proxy = DeviceProxy::new(&conn).await.expect("Cannot create proxy");
-    let percentage = proxy.percentage().await.expect("Cannot get percentage");
-    barc.lock().unwrap().set_battery_percentage(percentage as u32);
-    let mut stream = proxy.receive_percentage_changed().await;
-    while let Some(x) = stream.next().await {
-        let percentage = x.get().await.expect("Cannot get percentage");
-        barc.lock().unwrap().set_battery_percentage(percentage as u32);
+    battery.percentage = proxy.percentage().await.unwrap() as usize;
+    barc.lock().unwrap().set_battery(battery);
+    let mut percentage_stream = proxy.receive_percentage_changed().await;
+    let mut state_stream = proxy.receive_state_changed().await;
+    loop {
+        tokio::select! {
+            Some(v) = percentage_stream.next() =>
+                battery.percentage = v.get().await.unwrap() as usize,
+            Some(v) = state_stream.next() =>
+                battery.charging = v.get().await.unwrap() == 1
+        }
+        barc.lock().unwrap().set_battery(battery);
     }
 }
 
